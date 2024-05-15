cfa9a3314bc5bd971677425a39f2321ff098ca6e
Torinosuke <Dianthus_ZaDarL@outlook.jp>
2024-05-15 18:30:34 +0900

    page miss

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Hello', { title: 'Satisfactions 26F5' });
+  res.render('hello', { title: 'Satisfactions 26F5' });
 });
 
 module.exports = router;
